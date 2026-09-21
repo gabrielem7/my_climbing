b12d100f5391d73569e033c70165613a60366e7e
gabrielem7 <159136131+gabrielem7@users.noreply.github.com>
2026-09-21 15:19:09 +0200

    fix axis

--- climbing_app.py
+++ climbing_app.py
@@ -544,6 +544,7 @@ if not df_bonus.empty:
                             category_orders={'max_grade': list_grades_grouped})
     fig_bonus_max.update_layout(margin=dict(l=0, r=0, t=40, b=0), legend=dict(orientation="h", yanchor="top", y=-0.2, xanchor="center", x=0.5, title=""))
     fig_bonus_max.update_yaxes(categoryorder='array', categoryarray=list_grades_grouped)
+    fig_bonus_max.update_xaxes(categoryorder='category ascending')
     st.plotly_chart(fig_bonus_max, use_container_width=True)
     # GRAFICO 2: Volume (Numero di tiri) nel tempo
     df_bonus_vol = df_bonus.groupby([time_col, 'climbing_type']).size().reset_index(name='count')
